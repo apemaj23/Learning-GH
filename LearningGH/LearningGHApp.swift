90cbb25225366c34f6a9edd2e30cda689aa80fa2
Arvil Pemaj <arvilpemaj@gmail.com>
2022-08-18 16:33:32 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Arvil Pemaj on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
